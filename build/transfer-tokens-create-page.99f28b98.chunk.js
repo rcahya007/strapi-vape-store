9b41c01b8364064bd38eaad68add632e2d474c89
Rendy Cahya <89686508+rendycahya@users.noreply.github.com>
2023-08-07 13:58:59 +0700

    v1 update build

---
+++ build/transfer-tokens-create-page.99f28b98.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkvape_market=self.webpackChunkvape_market||[]).push([[4816],{81524:(O,t,e)=>{e.r(t),e.d(t,{default:()=>a});var s=e(67294),E=e(80120),n=e(86706),_=e(36364),r=e(43467);const a=()=>{const P=(0,n.v9)(_._);return s.createElement(E.O4,{permissions:P.settings["transfer-tokens"].create},s.createElement(r.Z,null))}}}]);
---
+++ build/transfer-tokens-edit-page.92867fd3.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkvape_market=self.webpackChunkvape_market||[]).push([[2492],{20289:(O,t,e)=>{e.r(t),e.d(t,{default:()=>a});var s=e(67294),E=e(80120),n=e(86706),_=e(36364),r=e(43467);const a=()=>{const P=(0,n.v9)(_._);return s.createElement(E.O4,{permissions:P.settings["transfer-tokens"].read},s.createElement(r.Z,null))}}}]);
